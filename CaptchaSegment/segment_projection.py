ed5a5f52278a8d0671bfc8cd037a30e90e4f6e61
chaoxiongtian <ambition_x@163.com>
2018-12-11 17:05:25 +0800

    投影分割算法

--- CaptchaSegment/segment_projection.py
+++ CaptchaSegment/segment_projection.py
@@ -1,27 +1,14 @@
 # -*- coding: utf-8 -*-
-# @Time    : 18-12-6 下午9:52
+# @Time    : 18-12-11 下午4:36
 # @Author  : MaxCentaur
 # @Email   : ambition_x@163.com
-# @File    : segment_projection
+# @File    : segment_pro.py
 # @Software: PyCharm
 
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 from segment_utils import *
-
-
-# 画出像素直方图 point_list 为纵坐标统计的像素值
-def draw(point_list):
-    print(point_list)
-    plt.figure(figsize=(12, 6))
-    plt.plot(point_list, label='projection_x')
-    my_x_ticks = np.arange(0, len(point_list), 5)
-    plt.xticks(my_x_ticks)
-    a_x = plt.gca()
-    for tick in a_x.xaxis.get_major_ticks():
-        tick.label1.set_fontsize(fontsize=5)
-    plt.show()
 
 
 # 统计纵坐标像素值
@@ -38,7 +25,22 @@ def get_vertical_counts(image):
     return result
 
 
-# 对投影统计值进行预处理 由[num1,num2,num3]变为[[index1,frequent1,num1],[index2,frequent2,num2]]
+# 统计横坐标像素值（分割之后处理用）
+def get_horizontal_counts(image):
+    pix_data = image.load()
+    image_w, image_h = image.size
+    result = []
+    for iter_y in range(image_h):
+        black = 0
+        for iter_x in range(image_w):
+            if pix_data[iter_x, iter_y] == 0:
+                black += 1
+        result.append(black)
+    return result
+
+
+# 对投影统计值进行预处理
+# 由[num1,num2,num3]变为[[index1,frequent1,num1],[index2,frequent2,num2]]
 def preprocessed(vertical_counts):
     count = 1
     num = vertical_counts[0]
@@ -56,15 +58,28 @@ def preprocessed(vertical_counts):
     return result
 
 
-# 找到波谷是0的位置，按照这个位置进行划分。
-def find_zero_point(segments):
+# 修正 第一个和最后一个 坐标信息。
+def revise_segments(segments):
     # 微调开始和终止位置
     segments[0][1] = 1
     if segments[-1][2] == 0:
         tmp = segments[-1][1]
         segments[-1][0] = segments[-1][0] - tmp + 1
         segments[-1][1] = 1
+    return segments
 
+
+# image的区域拷贝 分割操作。
+def segment(image, x_1, y_1, x_2, y_2):
+    box = (x_1, y_1, x_2, y_2)
+    region = image.crop(box)
+    return region
+
+
+# 找到波谷是0的位置，按照这个位置进行划分。
+def find_zero_point(segments):
+    # 微调开始和终止位置
+    segments = revise_segments(segments)
     # 找num = 0
     (index, frequent, num) = segments[0]
     boundary = [[0, index, frequent, num]]
@@ -73,10 +88,9 @@ def find_zero_point(segments):
             boundary.append([i, index, frequent, num])
     (index, frequent, num) = segments[-1]
     boundary.append([len(segments) - 1, index, frequent, num])
-    # print(boundary)
 
     edge = []
-    edge_info = []
+    edge_segments = []
     for i in range(1, len(boundary)):
         start = boundary[i - 1][1]
         end = boundary[i][1] - boundary[i][2] + 1
@@ -84,41 +98,9 @@ def find_zero_point(segments):
         tmp = segments[boundary[i - 1][0]:boundary[i][0]]
         if i == (len(boundary) - 1):
             tmp = segments[boundary[i - 1][0]:boundary[i][0] + 1]
-        edge_info.append(tmp)
-    return edge, edge_info
-
-
-# 获得分割区间
-def get_project_segment_info(image):
-    # 1.获得垂直投影值
-    vertical_counts = get_vertical_counts(image)
-    # draw(vertical_project)  # 画出折线图
-    print("image width is %d " % len(vertical_counts))
-
-    # 2.对竖直方向的像素统计值进行预处理
-    segments = preprocessed(vertical_counts)
-
-    # 3.找0的位置进行分割.
-    edge, wave_info = find_zero_point(segments)
-
-    return edge, wave_info
-
-
-def segment(image, x_1, y_1, x_2, y_2):
-    """
-    图片拷贝函数
-    """
-    box = (x_1, y_1, x_2, y_2)
-    region = image.crop(box)
-    return region
-
-
-def get_images(image, edge):
-    images = []
-    [_, height] = image.size
-    for i, [start, end] in enumerate(edge):
-        images.append(segment(image, start, 0, end, height))
-    return images
+        edge_segments.append(tmp)
+    print(edge_segments)
+    return edge, edge_segments
 
 
 # 长度过于窄的直接进行清理。
@@ -180,12 +162,12 @@ def find_area_min_trough(trough_list, start, middle, end):
 
 
 # 对部分区域按照 期望分割的数量 进行分割
-def get_sub_edge(each_edge, wave_info, tar_num):
+def get_sub_edge(each_edge, edge_segments, tar_num):
     if tar_num == 1:
         return [each_edge]
 
     # 根据边的信息找波谷 troughs集合
-    troughs = find_min_point(wave_info)
+    troughs = find_min_point(edge_segments)
 
     # 从波谷列表中找到指定区域中最小的布谷
     start, end = each_edge
@@ -208,7 +190,7 @@ def get_sub_edge(each_edge, wave_info, tar_num):
 
 
 # 根据先验条件 对部分 区域再进行分割。
-def re_segment(edge, wave_info, pre_conditions):
+def re_segment(edge, edge_segments, pre_conditions):
     def get_tar_num(key):
         for j, each in enumerate(pre_conditions):
             if key < each:
@@ -221,16 +203,39 @@ def re_segment(edge, wave_info, pre_conditions):
         # 根据先验条件得到预分割的份数。
         tar_num = get_tar_num(width)
 
-        new_edge = new_edge + get_sub_edge(edge[i], wave_info[i], tar_num)
+        new_edge = new_edge + get_sub_edge(edge[i], edge_segments[i], tar_num)
     return new_edge
 
 
-def projection(file_path):
-    image = convert_binary(Image.open(file_path))
-    edge, wave_info = get_project_segment_info(image)
-    # 长度小于阈值的块进行去除
-    edge, wave_info = correct_segments(edge, wave_info)
+def get_images(image, edge):
+    images = []
+    [_, height] = image.size
+    for i, [start, end] in enumerate(edge):
+        images.append(segment(image, start, 0, end, height))
+    return images
 
+
+def projection(file_path, pre_conditions=(30, 60, 90)):
+    # TODO: 1.先水平投影，切割出上下白边。 2.再竖直投影，先找波谷为0的点 3.再按照先验证条件按照长度进行分割。
+    image = convert_binary(Image.open(file_path))
+
+    # 1.水平投影，切割上下白边。
+    horizontal_counts = get_horizontal_counts(image)
+    # 对像素统计值进行预处理
+    horizontal_segments = preprocessed(horizontal_counts)
+    horizontal_segments = revise_segments(horizontal_segments)
+    horizontal_start, horizontal_end = horizontal_segments[0][0], horizontal_segments[-1][0]
+    new_image = segment(image, 0, horizontal_start, image.size[0], horizontal_end)
+
+    # 2.竖直投影，找到波谷是0的位置进行分割。
+    vertical_counts = get_vertical_counts(new_image)
+    segments = preprocessed(vertical_counts)
+    # edge表示每个字符的所处的纵坐标的区间，edge_segments表示这个区间中所有预处理之后的像素值统计数据
+    edge, edge_segments = find_zero_point(segments)
+    # 长度小于阈值的块进行去除
+    edge, edge_segments = correct_segments(edge, edge_segments)
+
+    # 3. 根据先验证条件按照长度进行分割
     # TODO： 此处 根据先验条件 对过于大的区间再进行分割。
     # 比如
     # 长度在30之内认为必然只存在一个字符
@@ -239,9 +244,14 @@ def projection(file_path):
     # 长度大于90 则认为四个字符
     #
     # 多个字符的处理方式是：在等分点的左右指定范围内找最小的波谷。
-
-    pre_conditions = [30, 60, 90]  # 0-30(切一份)，30-60(切两份),60-90(切三分),>90(切四份)
-    edge = re_segment(edge, wave_info, pre_conditions)
-
-    images = get_images(image, edge)
+    # pre_conditions = [30, 60, 90]  # 0-30(切一份)，30-60(切两份),60-90(切三分),>90(切四份)
+    # pre_conditions_qq = [67, 106, 134]
+    edge = re_segment(edge, edge_segments, pre_conditions)
+    images = get_images(new_image, edge)
     return images
+
+
+file_path = '/home/tianchaoxiong/LinuxData/data/MasterExpData/after/qq/results_85/images/0.png'
+images = projection(file_path, pre_conditions=[67, 106, 134])
+for each in images:
+    each.show()
