f2f54d9cee5efc4ca1eae1f6cdf4231e98ec5f9b
chaoxiongtian <ambition_x@163.com>
2019-01-13 15:27:46 +0800

    修复百度2016验证码

--- CaptchaGenerate/generate_unit.py
+++ CaptchaGenerate/generate_unit.py
@@ -258,19 +258,20 @@ def generate_digg(captcha_name):
 def generate_baidu(captcha_name):
     # 构造captcha
     captcha = Captcha(
-        captcha_width=65,  # 验证码宽
-        captcha_high=30,  # 验证按高
+        captcha_width=130,  # 验证码宽
+        captcha_high=60,  # 验证按高
         have_bg=False,  # 是否有背景
         bg_folder=os.path.join(data_folder, "bg", captcha_name),  # 有背景的话，背景路径
         start_x=10,  # 第一个字符的开始位置
         start_x_random_range=10,
-        step=-1,  # 每个字符之间的距离
+        step=-3,  # 每个字符之间的距离
         step_stretch=1,  # 字符间距扩大每个字符之间的距离
         step_random_range=0,  # 字符之间距离随机的范围
         font_folder=os.path.join(data_folder, "font", captcha_name),  # 字体路径，多种字体直接全部读出来
         font_color=(0, 0, 0),  # 指定颜色(处理之后都需要二值化，所以可不用随机颜色)
-        font_size=30,  # 字体基准大小
-        font_size_random_range=0  # 字体随机范围
+        font_size=47,  # 字体基准大小
+        font_size_random_range=0,  # 字体随机范围
+        contours=True  # 绘制轮廓
     )
     captcha_feature = (None, None, None, None, None, (0, 0), (0, 0), 1, -15, 15, 0, 0, (0, 0, 0))
     return captcha, captcha_feature
