7acd3ca985153514ecc80d797c1c13c54c75d70a
chaoxiongtian <ambition_x@163.com>
2019-01-21 22:35:44 +0800

    add LeNet5 AlexNet GoogLenet

--- CaptchaNet/models/core_net.py
+++ CaptchaNet/models/core_net.py
@@ -5,6 +5,7 @@
 # @File    : core_net.py
 # @Software: PyCharm
 
+import torch
 import torch.nn as nn
 
 
@@ -72,7 +73,7 @@ class SimpleCnn3(nn.Module):
 
 
 class SimpleCnn5(nn.Module):
-    def __init__(self, channel=1, y_dim=10, keep_prob=0.75):
+    def __init__(self, channel=1, y_dim=10, keep_prob=0.5):
         super(SimpleCnn5, self).__init__()
         self.conv1 = nn.Sequential(
             nn.Conv2d(channel, 32, 3, 1, 1),
@@ -105,6 +106,165 @@ class SimpleCnn5(nn.Module):
         if _type == 'kaiming':
             for ms in self._modules:
                 kaiming_init(self._modules[ms].parameters())
+
+
+class LeNet5(nn.Module):
+    def __init__(self, channel=1, y_dim=10, keep_prob=0.5):
+        super(LeNet5, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(channel, 6, 5, padding=2),
+            nn.ReLU(), nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(6, 16, 5, 1, 0),
+            nn.ReLU(), nn.MaxPool2d(2),
+        )
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.out = nn.Linear(84, y_dim)
+
+    def forward(self, x):  # torch.Size([64, 1, 32, 32])
+        x = self.conv1(x)  # torch.Size([64, 6, 14, 14])
+        x = self.conv2(x)  # torch.Size([64, 16, 5, 5])
+        x = x.view(x.size(0), -1)  # torch.Size([64, 16*5*5])
+        x = self.fc1(x)  # torch.Size([64, 120])
+        x = self.fc2(x)  # torch.Size([64, 84])
+        output = self.out(x)  # torch.Size([64, y_dim])
+        return output
+
+    def weight_init(self, _type='kaiming'):
+        if _type == 'kaiming':
+            for ms in self._modules:
+                kaiming_init(self._modules[ms].parameters())
+
+
+class AlexNet(nn.Module):  # 输入为227*227
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
+        return x
+
+
+class Inception(nn.Module):
+    def __init__(self, in_planes, n1x1, n3x3red, n3x3, n5x5red, n5x5, pool_planes):
+        super(Inception, self).__init__()
+        # 1x1 conv branch
+        self.b1 = nn.Sequential(
+            nn.Conv2d(in_planes, n1x1, kernel_size=1),
+            nn.BatchNorm2d(n1x1),
+            nn.ReLU(inplace=True),
+        )
+
+        # 1x1 conv -> 3x3 conv branch
+        self.b2 = nn.Sequential(
+            nn.Conv2d(in_planes, n3x3red, kernel_size=1),
+            nn.BatchNorm2d(n3x3red),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(n3x3red, n3x3, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n3x3),
+            nn.ReLU(inplace=True),
+        )
+
+        # 1x1 conv -> 5x5 conv branch
+        self.b3 = nn.Sequential(
+            nn.Conv2d(in_planes, n5x5red, kernel_size=1),
+            nn.BatchNorm2d(n5x5red),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(n5x5red, n5x5, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n5x5),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(n5x5, n5x5, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n5x5),
+            nn.ReLU(inplace=True),
+        )
+
+        # 3x3 pool -> 1x1 conv branch
+        self.b4 = nn.Sequential(
+            nn.MaxPool2d(3, stride=1, padding=1),
+            nn.Conv2d(in_planes, pool_planes, kernel_size=1),
+            nn.BatchNorm2d(pool_planes),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        y1 = self.b1(x)
+        y2 = self.b2(x)
+        y3 = self.b3(x)
+        y4 = self.b4(x)
+        return torch.cat([y1, y2, y3, y4], 1)
+
+
+class GoogLeNet(nn.Module):
+    def __init__(self):
+        super(GoogLeNet, self).__init__()
+        self.pre_layers = nn.Sequential(
+            nn.Conv2d(3, 192, kernel_size=3, padding=1),
+            nn.BatchNorm2d(192),
+            nn.ReLU(inplace=True),
+        )
+
+        self.a3 = Inception(192, 64, 96, 128, 16, 32, 32)
+        self.b3 = Inception(256, 128, 128, 192, 32, 96, 64)
+
+        self.maxpool = nn.MaxPool2d(3, stride=2, padding=1)
+
+        self.a4 = Inception(480, 192, 96, 208, 16, 48, 64)
+        self.b4 = Inception(512, 160, 112, 224, 24, 64, 64)
+        self.c4 = Inception(512, 128, 128, 256, 24, 64, 64)
+        self.d4 = Inception(512, 112, 144, 288, 32, 64, 64)
+        self.e4 = Inception(528, 256, 160, 320, 32, 128, 128)
+
+        self.a5 = Inception(832, 256, 160, 320, 32, 128, 128)
+        self.b5 = Inception(832, 384, 192, 384, 48, 128, 128)
+
+        self.avgpool = nn.AvgPool2d(kernel_size=8, stride=1)
+        self.linear = nn.Linear(1024, 10)
+
+    def forward(self, inputs):
+        network = self.pre_layers(inputs)
+        network = self.a3(network)
+        network = self.b3(network)
+        network = self.maxpool(network)
+        network = self.a4(network)
+        network = self.b4(network)
+        network = self.c4(network)
+        network = self.d4(network)
+        network = self.e4(network)
+        network = self.maxpool(network)
+        network = self.a5(network)
+        network = self.b5(network)
+        network = self.avgpool(network)
+        network = network.view(network.size(0), -1)
+        out = self.linear(network)
+        return out, network
 
 
 # 反向传导的时候还是用kaiming优化
